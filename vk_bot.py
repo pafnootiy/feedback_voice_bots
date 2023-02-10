42c3cf28679f753ea580ed2f227d5873ea072616
pafnootiy <ksander-86@mail.ru>
2023-02-10 18:50:06 +0400

    echobot for VK.

--- vk_bot.py
+++ vk_bot.py
@@ -3,17 +3,39 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 vk_token =os.getenv("VK_TOKEN")
+# from vk_api.longpoll import VkLongPoll, VkEventType
+
+# # vk_session = vk_api.VkApi(token=vk_token)
+
+# # longpoll = VkLongPoll(vk_session)
+
+# # for event in longpoll.listen():
+# #     if event.type == VkEventType.MESSAGE_NEW:
+# #         print('Новое сообщение:')
+# #         if event.to_me:
+# #             print('Для меня от: ', event.user_id)
+# #         else:
+# #             print('От меня для: ', event.user_id)
+# #         print('Текст:', event.text)
+
+import random
+
+import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-vk_session = vk_api.VkApi(token=vk_token)
 
-longpoll = VkLongPoll(vk_session)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+
+if __name__ == "__main__":
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
