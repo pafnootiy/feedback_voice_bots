8a1e1a5986af4820d8f634083e2dabe2403966bb
pafnootiy <ksander-86@mail.ru>
2023-03-16 19:36:59 +0400

    hide path to json

--- create_intent.py
+++ create_intent.py
@@ -1,16 +1,13 @@
 import json
 import os
 
-
 from dotenv import load_dotenv
 from google.cloud import dialogflow
 
 
-def create_intent(phrases,project_id):
-
+def create_intent(phrases, project_id):
     intents_client = dialogflow.IntentsClient()
     parent = dialogflow.AgentsClient.agent_path(project_id)
-
 
     for display_name, issue in phrases.items():
         training_phrases = []
@@ -22,7 +19,6 @@ def create_intent(phrases,project_id):
         text = dialogflow.Intent.Message.Text(text=[issue["answer"]])
         message = dialogflow.Intent.Message(text=text)
 
-
         intent = dialogflow.Intent(
             display_name=display_name, training_phrases=training_phrases,
             messages=[message]
@@ -30,16 +26,17 @@ def create_intent(phrases,project_id):
         response = intents_client.create_intent(
             request={"parent": parent, "intent": intent}
         )
+
+
 def main():
     load_dotenv()
-
-    with open("training_phrases.json", "r", encoding="utf-8") as file:
+    path_to_phrases = os.getenv('PATH_TO_PHRASES')
+    with open(path_to_phrases + "training_phrases.json", "r", encoding="utf-8") as file:
         phrases_json = file.read()
     phrases = json.loads(phrases_json)
-    
     project_id = os.getenv("PROGECT_ID")
-    create_intent(phrases,project_id)
+    create_intent(phrases, project_id)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
